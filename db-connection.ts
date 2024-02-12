bbfd542bf9dbd381c0fbc305d009b72e2405487b
DESKTOP-A3RVKTM\Administrator <eslenderdark@gmail.com>
2024-02-12 10:06:18 +0100

    Añadido Cors + Actualizado de BDD

--- db-connection.ts
+++ db-connection.ts
@@ -1,11 +1,13 @@
 import { Pool } from 'pg';
+const connectionString = 'postgres://barberia_database_user:OGddn5xqz8C3gCem7EIF3yQ2PlftgOKS@dpg-cn4sih8l5elc73ctgje0-a/barberia_database'
 
 const pool = new Pool({
-    user: 'postgres',
+    connectionString
+   /* user: 'postgres',
     password: '1234',
     host: 'localhost',
     port: 5432, // default Postgrades port
-    database: 'barberia'
+    database: 'barberia'*/
 });
 
 export function query(text: any): any {
--- dist/db-connection.js
+++ dist/db-connection.js
@@ -2,12 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.query = void 0;
 var pg_1 = require("pg");
+var connectionString = 'postgres://barberia_database_user:OGddn5xqz8C3gCem7EIF3yQ2PlftgOKS@dpg-cn4sih8l5elc73ctgje0-a/barberia_database';
 var pool = new pg_1.Pool({
-    user: 'postgres',
-    password: '1234',
-    host: 'localhost',
-    port: 5432,
-    database: 'barberia'
+    connectionString: connectionString
+    /* user: 'postgres',
+     password: '1234',
+     host: 'localhost',
+     port: 5432, // default Postgrades port
+     database: 'barberia'*/
 });
 function query(text) {
     return pool.query(text);
