a57a1200b8cf0123cd6e1007e1335a49136c3441
DESKTOP-A3RVKTM\Administrator <eslenderdark@gmail.com>
2024-02-12 10:10:25 +0100

    añadido cors en index.js

--- dist/index.js
+++ dist/index.js
@@ -59,10 +59,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
 var db = __importStar(require("./db-connection"));
 var body_parser_1 = __importDefault(require("body-parser"));
-var app = express_1.default();
 var jsonParser = body_parser_1.default.json();
+var app = express_1.default();
+app.use(cors_1.default());
 app.get('/', function (req, res) {
     res.send('Hello from express and typescript');
 });
