ce5a884a3c68120b1967fd2b84fa418e6501062d
DESKTOP-A3RVKTM\Administrator <eslenderdark@gmail.com>
2024-02-13 08:42:22 +0100

    debug

--- dist/db-connection.js
+++ dist/db-connection.js
@@ -4,12 +4,12 @@ exports.query = void 0;
 var pg_1 = require("pg");
 var connectionString = 'postgres://barberia_database_user:OGddn5xqz8C3gCem7EIF3yQ2PlftgOKS@dpg-cn4sih8l5elc73ctgje0-a/barberia_database';
 var pool = new pg_1.Pool({
-    // connectionString
-    user: 'postgres',
+    connectionString: connectionString
+    /*user: 'postgres',
     password: '1234',
     host: 'localhost',
-    port: 5432,
-    database: 'barberia'
+    port: 5432, // default Postgrades port
+    database: 'barberia'*/
 });
 function query(text) {
     return pool.query(text);
